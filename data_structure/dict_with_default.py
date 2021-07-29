9e1fc99d2cc96d2c5bba7de69970fea1f0fb3e82
sj Li <mummy_li@163.com>
2021-07-29 09:19:04 +0800

    修改了一个警告

--- data_structure/dict_with_default.py
+++ data_structure/dict_with_default.py
@@ -2,7 +2,8 @@ from collections import defaultdict
 
 # 1、defaultdict会自动为将要访问的键（就算目前字典中并不存在这样的键）创建映射实体。
 d = defaultdict(list)
-# d = defaultdict(set)
+
+# 或者初始化为set d = defaultdict(set)
 d["a"].append(1)
 d["a"].append(2)
 d["b"].append(4)

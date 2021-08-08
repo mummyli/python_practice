b4a3b14bd020aebc0a0c44fb571b4fff535e03a0
sj L <mummy_li@163.com>
2021-08-08 22:50:15 +0800

    initial commit

---
+++ class_and_object/implementing_type_constraint.py
@@ -0,0 +1,69 @@
+from typing import Sized, Type
+
+
+class Descriptor:
+    def __init__(self, name=None, **opts) -> None:
+        self.name = name
+        for key, value in opts.items():
+            setattr(self, key, value)
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
+
+
+class Typed(Descriptor):
+    expected_type = type(None)
+
+    def __set__(self, instance, value):
+        if not isinstance(value, self.expected_type):
+            raise TypeError("expected " + str(self.expected_type))
+        
+        super().__set__(instance, value)
+
+
+class Unsigned(Descriptor):
+    def __set__(self, instance, value):
+        if(value<0):
+            raise ValueError("Expected >= 0")
+
+        super().__set__(instance, value)
+
+class MaxSized(Descriptor):
+    def __init__(self, name, **opts) -> None:
+        if "size" not in opts:
+            raise TypeError("missing size option")
+        super().__init__(name=name, **opts)
+
+    def __set__(self, instance, value):
+        if(len(value) >= self.size):
+            raise ValueError("size must be < " + str(self.size))
+        super().__set__(instance, value)
+
+
+class Integer(Typed):
+    expected_type = int
+
+class UnsignedInteger(Typed, Unsigned):
+    pass
+
+class Float(Typed):
+    expected_type = float
+
+class UnsignedFloat(Float, Unsigned):
+    pass
+
+class String(Typed):
+    expected_type = str
+
+class SizedString(String, MaxSized):
+    pass
+
+class Stock:
+    name = SizedString('name', size=8)
+    shares = UnsignedInteger('shares')
+    price = UnsignedFloat('price')
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
